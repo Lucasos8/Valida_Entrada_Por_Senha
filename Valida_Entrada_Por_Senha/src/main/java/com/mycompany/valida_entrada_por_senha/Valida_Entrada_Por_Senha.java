275ba04fd078176934a2702a2f07a229c07b08c0
Lucas Oliveira Souza <125596582+Lucasos8@users.noreply.github.com>
2025-05-21 22:56:23 -0300

    Primeiro commit
    Postando a classe vazia.

---
+++ Valida_Entrada_Por_Senha/src/main/java/com/mycompany/valida_entrada_por_senha/Valida_Entrada_Por_Senha.java
@@ -0,0 +1,6 @@
+package com.mycompany.valida_entrada_por_senha;
+
+public class Valida_Entrada_Por_Senha {
+
+   
+}
---
+++ Valida_Entrada_Por_Senha/src/test/java/com/mycompany/valida_entrada_por_senha/Valida_Entrada_Por_Senha.java
@@ -0,0 +1,8 @@
+package com.mycompany.valida_entrada_por_senha;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Valida_Entrada_Por_Senha {
+
+}
